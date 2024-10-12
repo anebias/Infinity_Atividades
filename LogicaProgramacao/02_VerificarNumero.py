1a66f7c8cb10851a0089933dafcb8067ecceb110
Alessandra Nébias <anebias@hotmail.com>
2024-10-12 19:16:22 -0300

    Resolução exercício 4 - Estrutura de repetição For

--- LogicaProgramacao/02_VerificarNumero.py
+++ LogicaProgramacao/02_VerificarNumero.py
@@ -1,3 +1,13 @@
+'''
+[LPIA-A02] Crie um programa em Python para verificar se um número é positivo, negativo ou zero. O programa deve solicitar ao usuário que insira um número e, em seguida, imprimir uma mensagem indicando a natureza do número.
+
+Se o número for maior que zero, exiba a mensagem "O número é positivo." Se for menor que zero, exiba "O número é negativo." Caso seja zero, a mensagem deve ser "O número é zero."
+
+Utilize estruturas condicionais e formatação com F-string para criar o programa de forma clara.
+'''
+
+# O exercício não deixa claro se é para colocar a f-string na mensagem de resposta, mas considerando a necessidade de utilização, coloquei a f-string para concatenar o número informado para avaliação.
+
 numero = float(input('Informe um número: '))
 
 if numero == 0:
