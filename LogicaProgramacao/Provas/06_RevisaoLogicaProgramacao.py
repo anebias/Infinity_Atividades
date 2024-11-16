f80991e3165057082415b29e864424fe05e9576a
Alessandra Nébias <anebias@hotmail.com>
2024-11-16 20:24:56 -0300

    correção revisao exercicio logica programacao

--- LogicaProgramacao/Provas/06_RevisaoLogicaProgramacao.py
+++ LogicaProgramacao/Provas/06_RevisaoLogicaProgramacao.py
@@ -2,25 +2,25 @@
 usuario_valido = 'usermain'
 senha_valida = 'senha1234'
 
-print('Menu de acesso \nVocê tem 3 tentativas')
+print('*** Menu de acesso *** \n*** Você tem 3 tentativas ***')
 
 usuario = input('Informe o seu usuário: ')
 senha = input('Informe a sua senha: ')
 
-tentativas = 3
+tentativa = 1
 
-while tentativas <=3 and tentativas > 0:
-    tentativas -= 1
+while tentativa <=3:
     validado = usuario == usuario_valido and senha == senha_valida
 
     if validado:
         print ('Seja bem vindo !')
         break
-    else:
-        print(f'Usuário ou senha inválidos. Você tem {tentativas} tentativas.')
+    elif tentativa < 3:
+        print(f'Usuário ou senha inválidos. Você tem {3 - tentativa} tentativas.')
         usuario = input('Informe o seu usuário: ')
         senha = input('Informe a sua senha: ')
-        
-if not validado:   
-    for i in range(3):
-        print('Acesso bloqueado')
+        tentativa += 1
+    else:
+        for i in range(3):
+            print('Acesso bloqueado')
+        break
